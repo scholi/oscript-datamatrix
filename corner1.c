3a11c29b22f934930fd93c603a2d89b136f1adbb
scholi <scholi@jabber.org>
2011-11-19 16:35:39 +0100

    merge corner[0-4].c into corners.c

--- corner1.c
+++
@@ -1,5 +0,0 @@
-#include "headers.h"
-void corner1(u c){
-	DBMSG("CORNER1");
-	MO((i<3)?nrow-1:(i!=3)*(i-4),(i<3)?i:ncol-1-(i==3));
-}
--- corner4.c
+++
@@ -1,5 +0,0 @@
-#include "headers.h"
-void corner4(u c){
-	DBMSG("CORNER4");
-	MO((i<2)?nrow-1:(i-2)/3,i*(ncol+(i-2)%3-3));
-}
