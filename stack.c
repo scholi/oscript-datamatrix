2c0282cb1a18f26bbd66a1eca3965b17b6e22601
Olivier Scholder <o.scholder@gmail.com>
2011-11-21 23:05:40 +0100

    Add ram to stack and for loop

--- stack.c
+++ stack.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #define SS(x) else if(s[i]==x)
 typedef unsigned int ui;
+typedef unsigned char u;
 
 ui sd[1024];
 ui lsd=0;
-unsigned char macro[1024];
-ui lmacro=0;
+u ram[1024000];
+u *ptr=ram;
+u macro[26][1024];
+ui lmacro[26];
 
-void Sinit(unsigned char* s){
+void Sinit(u* s){
 	for(ui i=0;s[i];i++){
 		if(s[i]=='x'){
 			ui v=0;
@@ -33,18 +36,30 @@ void Sinit(unsigned char* s){
 		SS('I'){ if(sd[--lsd]) lsd--; else sd[lsd-2]=sd[--lsd]; }
 		SS('D') sd[lsd++]=sd[lsd-1];
 		SS('C'){ ui l=sd[--lsd]; for(ui j=0;j<l;j++) sd[lsd++]=sd[lsd-l]; }
-		SS('['){ lmacro=0; for(++i;s[i]!=']';i++) macro[lmacro++]=s[i];}
-		SS('@'){ ui l=sd[--lsd]; macro[lmacro]=0; for(ui j=0;j<l;j++) Sinit(macro);}
+		SS('['){ u k=s[++i]-'a'; lmacro[k]=0; for(++i;s[i]!=']';i++) macro[k][lmacro[k]++]=s[i]; }
+		SS('@'){ ui l=sd[--lsd]; u k=s[++i]-'a'; macro[k][lmacro[k]]=0; for(ui j=0;j<l;j++) Sinit(macro[k]); }
 		SS('K') lsd=0;
-
+		SS('G') sd[lsd++]=*ptr;
+		SS('P') *ptr=sd[--lsd];
+		SS('Q') *(++ptr)=sd[--lsd];
+		SS('A')	ptr++;
+		SS('B') ptr--;
+		SS('E') ptr=ram+sd[--lsd];
+		SS('F'){ i++; ui j=sd[lsd-3],b=sd[lsd-2],c=sd[lsd-1];lsd-=3;for(;j<b;j+=c){ sd[lsd++]=j; Sinit(macro[s[i]-'a']);}}
 	}
 }
 
 #ifdef STANDALONE
 int main(_,__){
 	printf("Calculate first 10 fibonacci numbers\n");
-	Sinit("x1x1[x2C+]xa@");
-	for(ui j=0;j<10;j++) printf("%i\n",sd[j]);
+	Sinit("x1Px1Q[aGBGA+Q]xa@a");
+	printf("Stack [%i]\n=========\n",lsd);
+	for(ui j=0;j<lsd;j++) printf("%i\n",sd[j]);
+	printf("RAM\n===\n");
+	for(ui k=0;k<10;k++){
+		for(ui j=0;j<20;j++) printf("%i ",ram[10*k+j]);
+		printf("\n");
+	}
 	return 0;
 }
 #endif
