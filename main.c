6a5b4a74f128c4995c7b51061a95af16f4a08804
Olivier Scholder <o.scholder@hispeed.ch>
2011-11-22 23:46:55 +0100

    Add comments to oscript part

--- main.c
+++ main.c
@@ -20,26 +20,19 @@ main(ui iv,char **V){
 	
 /* Init log,alog in gallois */
 	DBMSG("Init alog/glog table...");
-	Sinit("x300Ex1Px0[Gx2*Dx12d^SDxff>IQZDx400G+Ex300-PE]xffx0rx400ExffPx0Q");
+	Sinit("x300Ex1Px0" // Init alog/glog with start value 0/1
+		"[Gx2*Dx12d^" // x12d = 301, la valeur a xorer si >256
+		"SDxff>I"
+		"QZDx400G+E" // goto glog position
+		"x300-" // calculate i (of alog[i])
+		"PE]xffx0rx400ExffPx0Q"); // run macro 255 times and put spcial value 0 and ff in glog
 	
-	DBMSG("Your input message is: %s",V[1]);
-#ifdef DEBUG
-	printf("Your input message [hex]: ");
-	f(ram[0]) printf("%x|",ram[i+1]);
-#endif
-	DBMSG("\nMessage length: %i",ram[0]);
-
 	encodeASCII(V[1]);
 
 	/* TotalSize,DataSize,RS Size,#Regions,#blocks */
 	ui* n=getSize(*ldata);
 
 	*ncol=*nrow=n[0];
-	DBMSG("Data-matrix size: %ix%i",*ncol,*nrow);
-	DBMSG("Datamatrix regions: %ix%i",n[3],n[3]);
-	DBMSG("Datamatrix capacity: %i",n[1]+n[2]);
-	DBMSG("Data size: %i",(*ldata));
-	DBMSG("Data capacity: %i",(n[1]));
 
 	/* Padd data	*/
 	 /* If not all the data fill de datamatrix, a 254 MC should be added to mark the end of data */
@@ -49,31 +42,11 @@ main(ui iv,char **V){
 		/*	Fill the free space with MC 129 */
 		while(*ldata<n[1]) data[(*ldata)++]=129;
 	}
-	DBMSG("Data size after padding: %i",*ldata);
 	/* Calculate Read-Solomon code	*/
 	RS(n[2]); 
-	DBMSG("Data size after RS: %i",*ldata);
-
-	/* display data */
-#ifdef DEBUG
-	printf("			");
-	f(*ldata) printf("%02i|",i);
-	printf("\nData: ");
-	f(*ldata) printf("%02x|",data[i]);
-	printf("\n");
-#endif
 
 	/* put data into matrix */
 	mapDataMatrix();
-	DBMSG("Maping: Done");
-
-	/* array debug */ 
-#ifdef DEBUG
-	f(*nrow){
-	for(ui j=0;j<*ncol;j++) printf("%03i|",array[*ncol*i+j]);
-	printf("\n");
-	}
-#endif
 
 	/* PS file creation */
 	ps();
