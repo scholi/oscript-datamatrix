8929838858062ce5daa0c3c0e9255efcc296eaac
Mathias Carlen <mathias.carlen@gmail.com>
2011-11-25 15:50:21 +0100

    utah, get args from stack

--- utah.c
+++ utah.c
@@ -26,7 +26,7 @@ void utah(int row,int col,u c){
 		"}x1&-"     // -=(0x25>>i)&1
 		"x6f"
 		"G"         // get counter i (ptr has not changed)
-		"}x1&-"     // -=(0x25>>i)&i (same as above, optimize!)
+		"}x1&-"     // -=(0x6f>>i)&i (same as above, optimize!)
 
 		// Build first argument of module
 		"S"         // switch second argument and col (c,arg2,row)
@@ -37,15 +37,13 @@ void utah(int row,int col,u c){
 		"x2-S"      // build true value
 		"G"         // get counter i
 		"x2<I"      // test and if
-
-		"x112EQQQ"  // put curr stack arg3=c,arg1,arg2 in memory
 		);
 
 		// get values from oscript back to c
 		// and call module
-		int arg1 = (int)(char)ram[0x113];
-		int arg2 = (int)(char)ram[0x114];
-		char arg3 = ram[0x115];
+		int arg1 = sd[--lsd];
+		int arg2 = sd[--lsd];
+		char arg3 = sd[--lsd];
 		module(arg1,arg2,arg3,7-i);
 	}
 
