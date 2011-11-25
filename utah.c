07ac035c695bb04360f8de435f6a44b988984db6
Mathias Carlen <mathias.carlen@gmail.com>
2011-11-25 15:27:20 +0100

    utah pushes function args on stack. get counter i optimized

--- utah.c
+++ utah.c
@@ -1,13 +1,15 @@
 #include "headers.h"
 
 void utah(int row,int col,u c){
-	ram[0x110] = c;
-	ram[0x111] = col&0xff;
-	ram[0x112] = row&0xff;
-  Sinit("x110EGAGAGA"); // push c,col,row on stack
+
+	// put function args on stack
+  sd[lsd++] = c;
+	sd[lsd++] = col;
+	sd[lsd++] = row;
 
 	for (int i=0;i<8;++i) {
 		
+		// TODO keep i on the stack
 		ram[0x113] = i;
 
 #if 0
@@ -20,21 +22,21 @@ void utah(int row,int col,u c){
 	  // Build second argument of module
 		"S"         // we need col
     "x25"       // push x25
-		"x113EG"    // get counter i
+		"x113EG"    // get counter i (set ptr)
 		"}x1&-"     // -=(0x25>>i)&1
 		"x6f"
-		"x113EG"    // get counter i
+		"G"         // get counter i (ptr has not changed)
 		"}x1&-"     // -=(0x25>>i)&i (same as above, optimize!)
 
 		// Build first argument of module
 		"S"         // switch second argument and col (c,arg2,row)
 		"D"         // copy row
-		"x113EG"    // get counter i
+		"G"         // get counter i
 		"x5<-"      // build false value
 		"S"         // swap to get a copy of row
 		"x2-S"      // build true value
-		"x113EG"    // get counter i
-		"x2<I"       // test and if
+		"G"         // get counter i
+		"x2<I"      // test and if
 
 		"x112EQQQ"  // put curr stack arg3=c,arg1,arg2 in memory
 		);
