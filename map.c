ccde8af1bf68e8aa0662b7a27a00da81f6831cc1
Mathias Carlen <mathias.carlen@gmail.com>
2011-11-26 02:36:15 +0100

    started map in oscript

--- map.c
+++ map.c
@@ -6,10 +6,29 @@ void mapDataMatrix(){
 	int row=4;
 	int col=0;
 	do{
+#if 1
+		// build stack c row col
+		verb=0;
+		sd[lsd++] = c;
+		sd[lsd++] = row;
+		sd[lsd++] = col;
+		Sinit( // stack : c row col
+		"(x0x8x1x6Fx1+)()" // c row col ()1 ()2
+		"x5x3R"       // ()1 ()2 c row col
+		"x0=S"        // ()1 ()2 c t1 row
+		"x100EG=&S"   // get nrow and compare, ... => ()1 ()2 t3 c
+		"x4x3RI"      // roll, s.t. : c ()1 ()2 t3, and if
+		"@"           // run ()1 or ()2 with c stil on stack
+		);
+		// pop c
+		c = sd[--lsd];
+#else
 		if((row==(*nrow)) && (col==0)) {
+			verb=1;
 			sd[lsd++] = c++;
-			Sinit("x0x8x1x6Fp");  // for i=0..8 corner1(...), pop c
+			Sinit("x0x8x1x6F.p");  // for i=0..8 corner1(...), pop c
 		}
+#endif
 		if((row==(*nrow)-2) && (col==0) && (*ncol%4)) {
 			sd[lsd++] = c++;
 			Sinit("x0x8x1x7Fp");  // for i=0..8 corner2(...), pop c
