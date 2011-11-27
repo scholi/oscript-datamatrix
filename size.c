a4664657d37d3ea841569ae2a3ace7a741dd0c8a
Olivier Scholder <o.scholder@gmail.com>
2011-11-27 01:27:35 +0100

    Finish getSize function

--- size.c
+++ size.c
@@ -2,7 +2,6 @@
 
 
 void getSize(ui l){
-	verb=1;
 	sd[lsd++]=l;
 	u* s = ram+0x1c8;
 	Sinit("x1c8DE" // goto right pos in RAM
@@ -10,33 +9,28 @@ void getSize(ui l){
 	"x1"	/* As no brakes exists in oscript,
 		 * we have to add a "True" value to be checked
 		 * @ beginning of the for and set it to 0 to break the for */
-	"x0[" // set macro to be run in the first loop
-	"x2zG" // (l i l s[i])
-	">(pAx1)(" // <= => !> (l i l>s[i]) (x1 is to continue to run the for)
+	"xac["
 	"x2*x8+D" // MS=matrix size ( l MS MS)
 	"GS" // DS=data size (l MS DS MS)
 	"D*x8/G-" // RS size = total size - data size (l MS DS RS=MS^2-s[i])
 	"x1" // # of regions
 	"x0" // Break code
-	")i" // perform if
+	"]" // perform if
+	"x0[" // set macro to be run in the first loop
+	"x2zG" // (l i l s[i])
+	">(pAx1)xaci" // <= => !> (l i l>s[i]) (x1 is to continue to run the for)
 	"]"
-	"xaa[px0]"); // pop i and add break code 0
-	f(9){ // loop for the single region
-		sd[lsd++]=i;
-		Sinit("S" // (l i break)
-		"x0xaai" // Run macro #0 defined above if for not breaked otherwise pop i
-		);
-	}
-	Sinit("x6x1Rp");
-	if(sd[--lsd]==0) return;
-	verb=0;
-	f(5){ // loop for the 2x2 regions
-		if(s[9+i]>=l){
-			sd[lsd++]=28+4*i; // matrix size
-			sd[lsd++]=s[9+i]; // data size
-			sd[lsd++]=(u)((28+4*i)*(28+4*i)/8)-s[9+i]; // RS size
-			sd[lsd++]=2; // # of regions
-			return;
-		}
-	}
+	"xaa[px0]" // pop i and add break code 0
+	"xab[Sx0xaai]" // Run macro #0 defined aoove if for not breaked otherwise pop i
+	"x0x9x1xabF" // For #1 (single region sizes)
+	"xac[" // set macro to be run in the first loop
+	"x4*x1c+D" // MS=matrix size ( l MS MS)
+	"GS" // DS=data size (l MS DS MS)
+	"D*x8/G-" // RS size = total size - data size (l MS DS RS=MS^2-s[i])
+	"x2" // # of regions
+	"x0" // Break code
+	"]"
+	"x0x5x1xabF" // For #2 (2x2 regions sizes)
+	"px5x1Rp"
+	);
 }
