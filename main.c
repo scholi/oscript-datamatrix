bbba484c6e66310ea9afa7327dcdef5937ce24a7
Mathias Carlen <mathias.carlen@gmail.com>
2011-11-30 23:49:43 +0100

    and some more

--- main.c
+++ main.c
@@ -1,13 +1,4 @@
-#define A "x111EG"
-#define B "x100EG"
-#define C "x101EG"
-#define D "x102EG"
-#define E "x200EG"
-#define F "x3x1R"
-#define H "x500+E"
-#define I "x111EPD"
 #define Y typedef unsigned
-#define Z "DDx0>Sx0=|Sx80<&SD"B"<Sx80>|&"
 Y char u;Y int ui;
 u*ptr;u ram[666];main(ui iv,u**x){
 ptr=ram;while(*x[1])*(++ptr)=*(x[1]++);*(ptr+1)=0;ram[0]=ptr-ram;
@@ -15,6 +6,7 @@ S(
 "x11[x111EPDx0x100EGx7-x111EG]"
 "x12[+DGSD*x8/G-x1x103EPBPBPBPx0]"
 "x13[&Sx4x3RI@x4x1Rpx3x2R]"
+"x14[DDx0>Sx0=|Sx80<&SDx100EG<Sx80>|&]"
 "x1[Zx3x2Rx2C*x0=x1S-x3x2RSx400+EGSx400+EG+xff%x300+EG*SE]"
 "x2[x2x4rx4x2Rx200+EGS}x1&x1+x3x2RSx100EG*+x500+EP]"
 "x3[Dx4Dx4x1R+x8%-Sx4x1R+x3x2R+]"
@@ -24,11 +16,11 @@ S(
 "x7[x11@Dx7=-+x111EGx3<Ix100EGx111EGx3-+x111EGx7=x111EGx3<Ix7G-x4x3RSx2@]"
 "x8[x11@x3=-x111EGx3<Ix100EGx111EGx3-+x111EGDx4=~x1&Sx4-*x111EGx4<Ix7G-x4x3RSx2@]"
 "x9[x111EPDx111EGDx2-x3%x3-x100EG+*x100EGx1-x111EGx2-x3/x111EGx2<Ix7G-x4x3RSx2@]"
-"xa[x2CSxd@x4x2Rx100EGx3Cx3x1R*+x500+EGx0=x3x1Rx80<&x3x2Rx2C<x3x2Rpx80>|&I@x2-Sx2+Sx2CDDx0>Sx0=|Sx80<&SDx100EG<Sx80>|&xa()i]"
-"xb[x2Cx2Cx100EG*+x500+EGx0=x3x2RDDx0>Sx0=|Sx80<&SDx100EG<Sx80>|&&xd@ix2+Sx2-Sx2CSDDx0>Sx0=|Sx80<&SDx100EG<Sx80>|&xb()i]"
+"xa[x2CSxd@x4x2Rx100EGx3Cx3x1R*+x500+EGx0=x3x1Rx80<&x3x2Rx2C<x3x2Rpx80>|&I@x2-Sx2+Sx2Cx14@xa()i]"
+"xb[x2Cx2Cx100EG*+x500+EGx0=x3x2Rx14@&xd@ix2+Sx2-Sx2CSx14@xb()i]"
 "xc[x3C(x0x8x1x6Fx1+)()x5x3Rx0=Sx100EG="
-"x13@" "x3C(x0x8x1x7Fx1+)()x5x3Rx0=Sx100EGx2-=&Gx4%x0>"
-"x13@" "Sxa@x1+Sx3+Sxb@x3+Sx1+SSx2CDx100EG<Sx80>|SDx100EG<Sx80>||xc()i]"
+"x13@x3C(x0x8x1x7Fx1+)()x5x3Rx0=Sx100EGx2-=&Gx4%x0>"
+"x13@Sxa@x1+Sx3+Sxb@x3+Sx1+SSx2CDx100EG<Sx80>|SDx100EG<Sx80>||xc()i]"
 "xd[(x0x8x1x5Fx3x1Rx1+x3x2R)()]"
 "x300Ex1Px0[Gx2*Dx12d^SDxff>I"
 "QZDx400G+Ex300-PE]xffx0rx400ExffPx0Qxad[ZSx200EGx1+DPMPE]x0~x0EG("
