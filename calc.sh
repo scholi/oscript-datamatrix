13be5a816a4fa7f55f32ac686169de5eec491f5a
Mathias Carlen <mathias.carlen@gmail.com>
2011-12-02 00:32:18 +0100

    for some reason, this version of calc yields the correct char count

--- calc.sh
+++ calc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-white=' \n\t;'
+white=' \n\t'
 
-size1=`cat main.c | tr -d $white | wc | awk '{print $3}'`
-sizeSpaces=`cat main.c | tr -cd $white | wc | awk '{print $3}'`
+size1=`cat main.c | tr -d "$white" | wc | awk '{print $3}'`
+sizeSpaces=`cat main.c | tr -cd "$white" | wc | awk '{print $3}'`
 
-echo -e "main.c\t${size1}\t${sizeSpaces}"
-size1=`cat stack.c | tr -d $white | wc | awk '{print $3}'`
-sizeSpaces=`cat stack.c | tr -cd $white | wc | awk '{print $3}'`
+echo -e "main.c\t\t${size1}\t${sizeSpaces}"
+size1=`cat stack.c | tr -d "$white" | wc | awk '{print $3}'`
+sizeSpaces=`cat stack.c | tr -cd "$white" | wc | awk '{print $3}'`
 
-echo -e "stack.c\t${size1}\t${sizeSpaces}"
+echo -e "stack.c\t\t${size1}\t${sizeSpaces}"
 
-size1=`cat combined.c | tr -d $white | wc | awk '{print $3}'`
-sizeSpaces=`cat combined.c | tr -cd $white | wc | awk '{print $3}'`
+size1=`cat combined.c | tr -d "$white" | wc | awk '{print $3}'`
+sizeSpaces=`cat combined.c | tr -cd "$white" | wc | awk '{print $3}'`
 echo -e "combined.c\t${size1}\t${sizeSpaces}"
